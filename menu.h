4f4ff2e67bf0b948d1bd63778ff279e843759cec
Haris <rostislav.osvald@gmail.com>
2017-06-08 13:46:13 +0200

    documentation +refac

--- menu.h
+++ menu.h
@@ -34,11 +34,10 @@ void moveCursor(WINDOW * menu, unsigned int & currPick, const int moveDir, const
 
 tiles * surfaceTile();
 tiles * undergroundTile();
-tiles *** generateMap(const char * fileName);
+tiles *** generateMap(const char * path);
 
 tiles * resolveTile(const uint8_t tileType);
 tiles *** loadMap(const char * fileName);
-
 
 void startGame(WINDOW * menu, tiles *** map);
 
@@ -89,6 +88,12 @@ void menu(){
 	}
 }
 
+/**
+ * @brief initializes ncurses standard screen and sets attributes to desired values
+ * @details First initializes the screen, then noecho is set so user input isnt printed on the screen right away and
+ * probably at bad coordinates. Cbreak makes the program wait for user input every time getch or wgetch is called.
+ * curs_set(0) hides the cursor. If the terminal can draw colors, initialize them. Refresh draws empty screen.
+ */
 void initNcurses(){
 	initscr();
 	noecho();
@@ -99,6 +104,13 @@ void initNcurses(){
 	refresh();
 }
 
+/**
+ * @brief initializes menu
+ * @details Initializes menu window with sizes given in WINDOW_HEIGHT and WINDOW_WIDTH constants,
+ * the menu is in the middle of the screen. The keypad option in this window is set on to register
+ * function keys such as arrow keys.
+ * @return pointer to the menu window
+ */
 WINDOW * initMenu(){
 	WINDOW * menu = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 1, COLS/2-WINDOW_WIDTH/2);
 
@@ -106,6 +118,14 @@ WINDOW * initMenu(){
 	return menu;
 }
 
+/**
+ * @brief draws a menu given by parameter
+ * @details Clears the screen and prints all choices in the menuPick vector. The first one has reverted colors,
+ * because its the one currently being chosen.
+ * @param menu pointer to the menu window
+ * @param menuPicks "list" of the menu choices
+ * @param displayMode decides how far away from each other are lines supposed to be
+ */
 void drawMenu(WINDOW * menu, const vector<string> & menuPicks, const int displayMode){
 	werase(menu);
 	box(menu,0,0);
@@ -119,8 +139,19 @@ void drawMenu(WINDOW * menu, const vector<string> & menuPicks, const int display
 
 	for (size_t i = 1, size = menuPicks.size() ; i < size && i*displayMode < WINDOW_HEIGHT ; ++i)
 		mvwprintw(menu,i*displayMode+1,3,menuPicks[i].c_str());
+
+	wrefresh(menu);
 }
 
+/**
+ * @brief moves the "cursor" in the menu
+ * @details
+ * @param menu pointer to the menu window
+ * @param currPick position of the currently chosen item from the "list", its a reference because its being changed in this function
+ * @param moveDir direction the cursor is moving in (up -1, down 1)
+ * @param menuPicks "list" of the menu choices
+ * @param displayMode decides how far away from each other are lines supposed to be
+ */
 void moveCursor(WINDOW * menu,unsigned  int & currPick, const int moveDir, const vector<string> & menuPicks, const int displayMode){
 
 	mvwprintw(menu,currPick*displayMode+1,3,menuPicks[currPick].c_str());
@@ -181,13 +212,19 @@ tiles * undergroundTile() {
 	}
 }
 
-
-tiles *** generateMap(const char * fileName){
-	srand((unsigned int) time(0));
+/**
+ * @brief generates new map randomly
+ * @details Creates a new directory with maps if there already isnt one, then opens a new file in this directory.
+ * Creates a new matrix of "random" tiles with desired chances and writes them to this file.
+ * @param path name of the newly generated file with a path to it (its in the map directory)
+ * @return pointer to the map matrix that has been generated, if its null, the generation has failed
+ */
+tiles *** generateMap(const char * path){
+	srand((unsigned int) time(0));//makes the tiles more "random"
 	mkdir("map", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
 	ofstream mapFile;
-	mapFile.open(fileName, ios::binary);
+	mapFile.open(path, ios::binary);
 	if(! mapFile.is_open() )
 		return NULL;
 	tiles ***map = new tiles**[20];
@@ -209,8 +246,28 @@ void startGame(WINDOW * menu, tiles *** map){
 
 }
 
+/**
+ * @brief lets player input name for his game, generates it and starts it
+ * @details The game is generated to the map directory (its created in generateMap if it doesnt exist).
+ * Player can input letters, numbers, spaces, dashes and underscores. Anything other than that
+ * is an illegal character and the player is warned with a warning message. Name also cant be empty
+ * and cant be longer than 25 characters, the player is warned in this case as well.
+ * Both of these warning messages are cleared on next passage of the loop where the input happens.
+ * The loop doesnt wait for the players input to continue, every 0.7 seconds it does that itself.
+ * This occurs for the cursor to blink so the player knows where he ended his input.
+ * If the input is a legal input character its written to the path array, printed to the screen
+ * and the cursor moves as well. This doesnt happen if the name is 25 characters long.
+ * If the player pressed the backspace key, the last character is erased from the screen
+ * and the cursor is moved. This doesnt happen when the name is 0 characters long.
+ * Its not deleted from the array because it will either be overwritten or it wont be in the final "string"
+ * because of the terminating character(\0) that is added after input is finished.
+ * The input ends when the player presses enter. Then the file is generated and that is reported to the player.
+ * If the generation didnt go as planned the player is warned and after hitting the enter key it goes back to the menu.
+ * Otherwise the game is started.
+ * @param menu pointer to the menu window
+ */
 void newGame(WINDOW * menu){
-	char fileName[30]="map/";
+	char path[30]="map/";
 	bool warning=false;
 	halfdelay(7);
 	mvwprintw(menu,1,1,"Input name of map:");
@@ -221,14 +278,14 @@ void newGame(WINDOW * menu){
 		char input = (char) wgetch(menu);
 		if(warning) {
 			for (int j = 1; j < 39; ++j)
-				mvwprintw(menu, 8, j , " ");
+				mvwprintw(menu, 8, j , " ");//clears whole line 8 except menus border.
 			warning=false;
 		}
 
 		if( isalpha(input) || isdigit(input) || input==' ' || input=='_' || input=='-' ) {
-			if(pos!=28) {
+			if(pos!=29) {
 				mvwprintw(menu, 2, (pos-4) + 1 , "%c", input);
-				fileName[pos] = input;
+				path[pos] = input;
 				++pos;
 			}
 			else{
@@ -247,15 +304,15 @@ void newGame(WINDOW * menu){
 				warning=true;
 				continue;
 			}
-			fileName[pos]='\0';
+			path[pos]='\0';
 			break;
 		}
-		else if(input!=ERR){
+		else if(input!=ERR){ //ERR is generated when the player doesnt input anything within the halfdelay time period
 			mvwprintw(menu, 8,1, "You cant use that character");
 			warning=true;
 		}
 
-		if(i%2)
+		if(i%2)		//produces the blink effect when the player doesnt input anything
 			mvwprintw(menu,2,pos-4+1," ");
 		else
 			mvwprintw(menu,2,pos-4+1,"_");
@@ -267,7 +324,7 @@ void newGame(WINDOW * menu){
 	mvwprintw(menu,2,2,"Generating map...");
 	wrefresh(menu);
 
-	tiles *** map = generateMap(fileName);
+	tiles *** map = generateMap(path);
 	if(!map){
 		mvwprintw(menu,2,2,"Error! File could not be generated.");
 		wattron(menu, A_REVERSE);
@@ -287,6 +344,11 @@ void newGame(WINDOW * menu){
 	startGame(menu, map);
 }
 
+/**
+ * @brief makes a tile from given tileType_t
+ * @param tileType type of tile with designated value
+ * @return pointer to newly created tileType, NULL if wrong number has been given
+ */
 tiles * resolveTile(const uint8_t tileType){
 	switch(tileType){
 		case AIR:
@@ -304,6 +366,13 @@ tiles * resolveTile(const uint8_t tileType){
 	}
 }
 
+/**
+ * @brief loads map matrix from given file
+ * @details Opens file in map directory for read, creates a new matrix of tiles pointers and fills them
+ * with pointers to types read from the file.
+ * @param fileName name of the file the game is loaded from
+ * @return pointer to newly created tile matrix, NULL if something went wrong (file couldnt be open, there was illegal value inside)
+ */
 tiles *** loadMap(const char * fileName) {
 	string path = "map/";
 	path+=fileName;
@@ -336,6 +405,17 @@ tiles *** loadMap(const char * fileName) {
 	return map;
 }
 
+/**
+ * @brief lets player pick a file with saved game from map folder
+ * @details First the function searches through the map directory and adds all files to the files vector.
+ * If the directory doesnt exist the vector will be empty as expected. In this list are exluded all files
+ * starting with '.' which includes mainly systems '.' and '..' but also hidden files. The player cant input
+ * file starting with '.' because its an illegal character. Menu with all these files is drawn upon the screen,
+ * one item per line. The player then chooses from these items with the use of arrow keys up and down and confirms
+ * his choice with enter key, after which the file is attempted to load. If it fails the the player is warned
+ * and after hitting the enter key it goes back to the menu. Otherwise the game is started.
+ * @param menu pointer to the menu window
+ */
 void loadGame(WINDOW * menu){
 	box(menu,0,0);
 	vector<string> files;
@@ -352,7 +432,7 @@ void loadGame(WINDOW * menu){
 
 	tiles *** map;
 	unsigned int currPick=0;
-	while(true) {
+	while(true) { //todo this could be merged to one unified function with the menu (it would return currPick)
 		int input = wgetch(menu);
 		if (input == KEY_UP && currPick > 0)
 			moveCursor(menu, currPick, MOVE_UP, files, ALL_LINES);
@@ -389,10 +469,18 @@ void loadGame(WINDOW * menu){
 	startGame(menu, map);
 }
 
+/**
+ * @brief tells the player how to control this game
+ * @param menu pointer to the menu window 
+ */
 void controls(WINDOW * menu){
-
+//todo just write all controls
 }
 
+/**
+ * @brief erases all memory from the menu window and closes it
+ * @param menu pointer to the menu window that is supposed to be closed
+ */
 void closeMenu(WINDOW * menu){
 	werase(menu);
 	wrefresh(menu);
